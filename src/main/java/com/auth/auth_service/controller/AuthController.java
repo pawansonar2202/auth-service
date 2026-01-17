b1a3ad1c3dd123264b225878ad2a4bc2d9e776ac
Pawan <pawanpsonar77@gmail.com>
2026-01-17 17:43:26 +0530

    Added Controller for auth

--- src/main/java/com/auth/auth_service/controller/AuthController.java
+++ src/main/java/com/auth/auth_service/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.auth.auth_service.controller;
 
+import com.auth.auth_service.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, String>> login (HttpServletRequest httpServletRequest)
+    {
+        String auth = httpServletRequest.getHeader("Authorization");
+
+        String jwtToken = authService.login(auth);
+
+        return ResponseEntity.ok(Map.of("token",jwtToken));
+
+    }
 }
